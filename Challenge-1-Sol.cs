ff096e920a89da1e1a60fdccffca29f772bf6ddb
Kavya Chowdary <40886679+KvyChwdry@users.noreply.github.com>
2025-02-21 12:17:37 +0530

    Update Challenge-1-Sol.cs

--- Challenge-1-Sol.cs
+++ Challenge-1-Sol.cs
@@ -1,1 +1,37 @@
+using System;
+using System.Collections.Generic;
 
+namespace ClockSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Grand Clock Tower time
+            TimeSpan grandClockTime = new TimeSpan(15, 0, 0);
+
+            // List of clock times around the town
+            List<TimeSpan> clockTimes = new List<TimeSpan>
+            {
+                new TimeSpan(14, 45, 0),
+                new TimeSpan(15, 5, 0),
+                new TimeSpan(15, 0, 0),
+                new TimeSpan(14, 40, 0)
+            };
+
+            // Calculate the time differences
+            List<int> timeDifferences = new List<int>();
+            foreach (var clockTime in clockTimes)
+            {
+                timeDifferences.Add((int)(clockTime - grandClockTime).TotalMinutes);
+            }
+
+            // Output the results
+            Console.WriteLine("Time differences (in minutes) from the Grand Clock Tower:");
+            for (int i = 0; i < timeDifferences.Count; i++)
+            {
+                Console.WriteLine($"Clock {i + 1}: {timeDifferences[i]} minutes");
+            }
+        }
+    }
+}
